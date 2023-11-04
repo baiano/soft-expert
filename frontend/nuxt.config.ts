82777e240be18b1ec7d4a613e058c059f9d8581e
Guilherme L. Chaves <guilhermelchaves@gmail.com>
2023-11-04 11:50:43 -0300

    Adding pinia and a default store added plugins for eslint and pinia on nuxt

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    [
+      '@pinia/nuxt',
+      {
+        autoImports: ['defineStore', 'acceptHMRUpdate', 'storeToRefs'],
+      }
+    ],
+  ],
 })
