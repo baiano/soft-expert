420b8b1d2ba5f46f685a50815cbe1c5f7c22b62b
Guilherme L. Chaves <guilhermelchaves@gmail.com>
2023-11-07 14:48:59 -0300

    Adding seeds

---
+++ seed/soft-expert-order_products.sql
@@ -0,0 +1,13 @@
+INSERT INTO public.order_products (id_order,id_product,quantity) VALUES
+	 (1,3,4),
+	 (1,2,1),
+	 (8,3,30),
+	 (9,3,6),
+	 (9,4,2),
+	 (10,1,2),
+	 (10,4,4),
+	 (11,3,3),
+	 (11,3,1),
+	 (12,3,1);
+INSERT INTO public.order_products (id_order,id_product,quantity) VALUES
+	 (13,6,1);
---
+++ seed/soft-expert-products.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.products (product,id_type,price) VALUES
+	 ('Crispy Fried Onions',1,3.47),
+	 ('Milk',1,1.99),
+	 ('Beer',5,2.49),
+	 ('Rock T-Shirt',4,12.99),
+	 ('Bananas',1,1.49),
+	 ('Magic beans',6,5.99),
+	 ('Potatoes',1,2.79);
