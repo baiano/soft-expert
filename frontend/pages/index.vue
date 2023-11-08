c6b9e7958dcbc31740a0e70a20bdf5c75992faeb
Guilherme L. Chaves <guilhermelchaves@gmail.com>
2023-11-08 17:09:16 -0300

    Removing widget from dash

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -20,27 +20,6 @@ onMounted(async () => {
     <DashboardInfoBox :icon="'i-heroicons-shopping-cart-20-solid'" :number="products.length" title="Total Products:" />
     <DashboardInfoBox :icon="'i-heroicons-shopping-cart-20-solid'" :number="orders.length" title="Orders:" />
 
-    <UCard class="col-span-2">
-      <div class="flex items-center">
-        <UIcon name="history" class="text-3xl" />
-        <span class="text-2xl font-bold">Last products added: </span>
-      </div>
-      <ul>
-        <li v-for="product in products.slice(-5).reverse() " :key="product.id">
-          {{ product.product }}
-        </li>
-      </ul>
-      <!-- see all -->
-      <div class="flex justify-end">
-        <ULink
-          class="text-sm font-bold"
-          :to="{ name: 'products' }"
-          :label="'See all'"
-        >
-          See all
-        </ULink>
-      </div>
-    </UCard>
   </div>
 </template>
 
