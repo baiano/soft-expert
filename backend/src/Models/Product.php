b97f15a4dc67f39fad455d3e6e023e1bb90a8d64
Guilherme L. Chaves <guilhermelchaves@gmail.com>
2023-11-13 22:26:23 -0300

    Validation on backend models

--- backend/src/Models/Product.php
+++ backend/src/Models/Product.php
@@ -11,6 +11,16 @@ class Product extends Model
         'type' => ['foreign_key' => 'id_type']
     ];
 
+    static $validates_presence_of = [
+        ['product'],
+        ['price'],
+        ['id_type']
+    ];
+
+    static $validates_numericality_of = [
+        ['price', 'greater_than' => 0]
+    ];
+
     public function calculateTax() {
         return $this->price * $this->type->tax;
     }
